b235b19b0039a1b58a934baed5fa6ecd198ad2aa
DeveKoke <jorge.be86@gmail.com>
2023-05-02 22:09:36 +0200

    soluciones

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
 console.log(document.title);
+//  * 1
+document.getElementById("gen-1").innerHTML = 'Generasión Pokimon';
+
+//  * 2
+document.getElementsByClassName("infocard-list")[0].style.backgroundColor = 'pink';
+'green';
+
+//  * 3
+var URLactual = window.location;
+console.log(URLactual);
+
+//  * 4
+
+//  * 5
+const imagenes = document.querySelectorAll(".img-sprite");
+for (let i = 0; i < imagenes.length; i++) {
+
+    console.log(imagenes[i].src);  
+}
+
+//  * 6
+const images = document.querySelectorAll(".img-sprite");
+for (let i = 0; i < images.length; i++) {
+
+    imagenes[i].src = 'https://media.giphy.com/media/2v170e71aanfi/giphy.gif';  
+}
+
+// * 7
+let flyingType = document.getElementsByClassName("infocard-lg-data text-muted")
+for (let i = 0; i < flyingType.length; i++) {
+        flyingType[i].style.backgroundColor = 'blue';
+}
+
+
